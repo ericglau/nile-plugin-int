81be649926a171063613a1753b9389a69a875473
Eric Lau <ericglau@outlook.com>
2022-10-24 15:22:29 -0400

    Use int - causes error

--- nile_upgrades/deploy_proxy.py
+++ nile_upgrades/deploy_proxy.py
@@ -1,6 +1,6 @@
 import click
 
 @click.command()
-@click.argument("myarg", type=str)
+@click.argument("myarg", type=int)
 def deploy_proxy(myarg):
     print(f"result {myarg}")
--- tests/resources/scripts/script.py
+++ tests/resources/scripts/script.py
@@ -1,3 +1,3 @@
 
 def run(nre):
-    nre.deploy_proxy(["1234"])
+    nre.deploy_proxy([1234])
